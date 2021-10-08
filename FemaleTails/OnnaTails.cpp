4a48ae20807bda5b79cc48b2d5b1cc35d480adfe
Kaori <super_so_nic1@hotmail.com>
2021-10-08 16:13:48 -0500

    Adding GUI.

--- FemaleTails/OnnaTails.cpp
+++ FemaleTails/OnnaTails.cpp
@@ -3,6 +3,30 @@
 #define ReplacePVM(a, b) helperFunctions.ReplaceFile("system\\" a ".PVM", "system\\" b ".PVM");
 NJS_MATERIAL* TemporaryMaterialArray[] = { nullptr };
 
+static Trampoline* LoadRegObjTextures_t = nullptr;
+static Trampoline* FreeRegObjTexlists_t = nullptr;
+
+static NJS_TEXNAME GUI_TEXNAME[2] = {};
+static NJS_TEXLIST GUI_TEXLIST = { arrayptrandlengthT(GUI_TEXNAME, int) };
+
+static void __cdecl LoadRegObjTextures_r(int a1)
+{
+	LoadPVM("GUI", &GUI_TEXLIST);
+	((decltype(LoadRegObjTextures_r)*)LoadRegObjTextures_t->Target())(a1);
+}
+
+static void __cdecl FreeRegObjTexlists_r()
+{
+	njReleaseTexture(&GUI_TEXLIST);
+	((decltype(FreeRegObjTexlists_r)*)FreeRegObjTexlists_t->Target())();
+}
+
+void Icons_Init()
+{
+	LoadRegObjTextures_t = new Trampoline(0x4212E0, 0x4212E5, LoadRegObjTextures_r, false);
+	FreeRegObjTexlists_t = new Trampoline(0x420F40, 0x420F45, FreeRegObjTexlists_r, false);
+}
+
 uint16_t Tails_UpperArmIndices_DC[] = {
 	0, 2,
 	4, 6,
@@ -531,6 +555,7 @@ extern "C"
 	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
 		Init_Tails();
+		Icons_Init();
 		ReplacePVM("Miles", "OnnaTails")
 	}
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
